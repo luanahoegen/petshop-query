1e1660fc4ea6b1bcab9ab8d96f967f976437b883
Luana Hoegen <luanahoegen123@gmail.com>
2026-04-17 18:17:44 -0300

    first commit

---
+++ src/main/java/com/udesc/account/framework/AnimalResource.java
@@ -0,0 +1,22 @@
+package com.udesc.account.framework;
+
+import com.udesc.account.common.dto.AnimalDTO;
+import com.udesc.account.common.service.GetAnimal;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+
+public class AnimalResource {
+
+    private final GetAnimal getAnimal;
+
+    public AnimalResource(GetAnimal getAnimal) {
+        this.getAnimal = getAnimal;
+    }
+
+    @GET
+    @Path("/{id}")
+    public AnimalDTO buscarPorId(@PathParam("id") Long id) {
+        return getAnimal.buscarPorId(id);
+    }
+}
---
+++ src/main/java/com/udesc/account/framework/ClienteResource.java
@@ -0,0 +1,22 @@
+package com.udesc.account.framework;
+
+import com.udesc.account.common.dto.ClienteDTO;
+import com.udesc.account.common.service.GetCliente;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+
+public class ClienteResource {
+
+    private final GetCliente getCliente;
+
+    public ClienteResource(GetCliente getCliente) {
+        this.getCliente = getCliente;
+    }
+
+    @GET
+    @Path("/{id}")
+    public ClienteDTO buscarPorId(@PathParam("id") Long id) {
+        return getCliente.buscarPorId(id);
+    }
+}
